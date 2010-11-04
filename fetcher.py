5875d51c80f73dd47015919d15a6d52f2cfc6ca5
Giuseppe Attardi <attardi@semawiki.di.unipi.it>
2010-11-04 16:25:14 +0100

    revert to sqlite3

--- fetcher.py
+++ fetcher.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #----------------------------------------------------------------------
-#       Copyright (c) 2010  Giuseppe Attardi (attardi@di.unipi.it).
+#	Copyright (c) 2010  Giuseppe Attardi (attardi@di.unipi.it).
 #----------------------------------------------------------------------
 
 """Feed Fetcher:
@@ -10,8 +10,8 @@ Usage:
   fetcher.py [options] file
 
 Options:
-  -d, --db              database file
-  -h, --help            display this help and exit
+  -d, --db		database file
+  -h, --help		display this help and exit
 """
 
 import sys
@@ -19,9 +19,7 @@ import getopt
 import time
 import feedparser
 import re
-from couchdbkit import Server, ResourceConflict, BulkSaveError
-
-store = Server('http://attardi:beppe@attardi.cloudant.com')['semawiki']
+from store import *
 
 def show_help():
     print >> sys.stderr, __doc__
@@ -44,11 +42,12 @@ def getAuthor(e, description):
     return author
 
 def main():
+
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], 'd:h', ['db', 'help'])
     except getopt.GetoptError:
         show_help()
-
+    
     db = 'newsflow'
     for opt, arg in opts:
         if opt in ('-h', '--help'):
@@ -65,6 +64,8 @@ def main():
         print "Can't open file", args[0]
         sys.exit(2)
 
+    store = StoreCreate(db)
+
     count = 0
     for url in list:
         if url[0] == '#':       # skip comments
@@ -74,49 +75,34 @@ def main():
         print feedTitle
         feedDescription = d.feed.get('description', u'')
         feedLink = d.feed.get('link', u'')
-        #print 'Storing channel...'
-        try:
-            ch = store.save_doc({
-                'type': 'channel',
-                'title': feedTitle,
-                '_id': feedLink,
-                'description': feedDescription
-            })
-        except (ResourceConflict, PreconditionFailed):
-            pass
-        #print 'Storing channel done.'
+        ch = Channel(feedTitle, feedLink, feedDescription)
         # FIXME: should be
-        #        ch = Channel(feedLink, feedTitle, feedDescription)
-        docs = []
+#        ch = Channel(feedLink, feedTitle, feedDescription)
         for e in d.entries:
+            if not 'id' in e:
+                if not e.link:
+                    continue
+                e.id = e.link
+            if store.get(Item, e.id):
+                continue
             count += 1
+            title = e.get('title', u'')
+            link = e.get('link', u'')
             description = e.get('description', u'')
             if 'date_parsed' in e:
                 t = int(time.mktime(e.date_parsed))
             else:
                 t = time.time()
-            docs.append({
-                '_id': e.id,
-                'type': 'item',
-                'channel': feedLink,
-                'title': e.get('title'),
-                'link': e.get('link'),
-                'description': description,
-                'time': t,
-                'enclosure': e.get('enclosures', [''])[0],
-                'category': e.get('category'),
-                'author': getAuthor(e, description)
-            })
-        #print 'Storing items...'
-        try:
-            store.bulk_save(docs)
-        except BulkSaveError, e:
-            print e
-        #print 'Storing item done.'
->>>>>>> Switched store to CouchDB/Cloudant.
+            enclosure = u''
+            if 'enclosures' in e:
+                enclosure = unicode(e.enclosures[0])
+            category = e.get('category', u'')
+            author = getAuthor(e, description)
+            item = Item(ch, e.id, title, link, description, t,
+                        enclosure, category, author)
+            store.add(item)
+    store.commit()
     print "Collected", count, "articles."
 
 if __name__ == '__main__':
     main()
-
-
